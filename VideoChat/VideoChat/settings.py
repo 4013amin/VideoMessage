9fdd32787ff35ae79beb46094b417423ccbabc06
delta <amin138400138400@gmail.com>
2025-05-19 11:38:18 +0330

    fix a bog

--- VideoChat/VideoChat/settings.py
+++ VideoChat/VideoChat/settings.py
@@ -25,9 +25,11 @@ INSTALLED_APPS = [
 ]
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",  # برای توسعه
-    },
+    "BACKEND": "channels_redis.core.RedisChannelLayer",
+    "CONFIG": {
+        "hosts": [("127.0.0.1", 6379)],
+    }
+
 }
 
 MIDDLEWARE = [
