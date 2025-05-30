fb21f87323caf9d3460500b33c75cc3942d977af
delta <amin138400138400@gmail.com>
2025-05-30 21:44:06 +0330

    Edit in urls for bog server

--- VideoChat/VideoChat/asgi.py
+++ VideoChat/VideoChat/asgi.py
@@ -1,12 +1,10 @@
 import os
-import django
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 import websocket.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VideoChat.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -15,4 +13,4 @@ application = ProtocolTypeRouter({
             websocket.routing.websocket_urlpatterns
         )
     ),
-})  
+})
